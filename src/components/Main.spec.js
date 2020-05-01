a6f9083f2184e4ed7ac8bfce5ff0bbb86f17d0ec
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-01 15:16:38 -0400

    confirm CSV loads from spec

--- src/components/Main.spec.js
+++ src/components/Main.spec.js
@@ -5,6 +5,9 @@ import Main from './Main'
 // hmm, why aren't styles applied when loading
 // import  "../style.scss"
 
+// we can import CSV files, since Webpack config has a loader for it
+import CsvData from "../mycsv.csv"
+
 describe('Main', () => {
   it('loads', () => {
     mount(<Main />, {
@@ -14,5 +17,15 @@ describe('Main', () => {
     cy.contains('Hello from react!').should('be.visible')
       // check if the style has been applied
       .and('have.css', 'color', 'rgb(255, 0, 0)')
+  })
+
+  it('loaded CSV correctly', () => {
+    expect(CsvData).to.have.length(5)
+    expect(CsvData[1]).to.deep.equal([
+      'Raja',
+      'Sem Corporation',
+      'pede.ultrices.a@tinciduntpedeac.co.uk',
+      'January 21st, 2019'
+    ])
   })
 })
